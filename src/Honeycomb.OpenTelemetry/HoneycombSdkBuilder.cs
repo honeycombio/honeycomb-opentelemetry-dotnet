1f13930ec452a1c7622920d4ad207b6842b7f029
Mike Goldsmth <goldsmith.mike@gmail.com>
2021-06-22 13:40:50 +0100

    update editorconfig to apply .NET conventions

--- src/Honeycomb.OpenTelemetry/HoneycombSdkBuilder.cs
+++ src/Honeycomb.OpenTelemetry/HoneycombSdkBuilder.cs
@@ -9,11 +9,11 @@ namespace Honeycomb.OpenTelemetry
     public class HoneycombSdkBuilder
     {
         public const string DefaultEndpointAddress = "https://api.honeycomb.io:443";
-        private Uri endpoint = new Uri(DefaultEndpointAddress);
-        private string apiKey;
-        private string dataset;
-        private Sampler sampler = new DeterministicSampler(1); // default to always sample
-        private ResourceBuilder resourceBuilder = ResourceBuilder.CreateDefault().AddEnvironmentVariableDetector();
+        private Uri _endpoint = new Uri(DefaultEndpointAddress);
+        private string _apiKey;
+        private string _dataset;
+        private Sampler _sampler = new DeterministicSampler(1); // default to always sample
+        private ResourceBuilder _resourceBuilder = ResourceBuilder.CreateDefault().AddEnvironmentVariableDetector();
 
         public HoneycombSdkBuilder WithEndpoint(string endpoint)
         {
@@ -22,60 +22,60 @@ namespace Honeycomb.OpenTelemetry
 
         public HoneycombSdkBuilder WithEndpoint(Uri endpoint)
         {
-            this.endpoint = endpoint;
+            _endpoint = endpoint;
             return this;
         }
 
         public HoneycombSdkBuilder WithAPIKey(string apiKey)
         {
-            this.apiKey = apiKey;
+            _apiKey = apiKey;
             return this;
         }
 
         public HoneycombSdkBuilder WithDataset(string dataset)
         {
-            this.dataset = dataset;
+            _dataset = dataset;
             return this;
         }
 
         public HoneycombSdkBuilder WithSampler(Sampler sampler)
         {
-            this.sampler = sampler;
+            _sampler = sampler;
             return this;
         }
 
         public HoneycombSdkBuilder WithSampleRate(uint sampleRate)
         {
-            sampler = new DeterministicSampler(sampleRate);
+            _sampler = new DeterministicSampler(sampleRate);
             return this;
         }
 
         public HoneycombSdkBuilder WithServiceName(string serviceName)
         {
-            resourceBuilder.AddService(serviceName);
+            _resourceBuilder.AddService(serviceName);
             return this;
         }
 
         public HoneycombSdkBuilder WithResourceAttributes(params KeyValuePair<string, object>[] attributes)
         {
-            resourceBuilder.AddAttributes(attributes);
+            _resourceBuilder.AddAttributes(attributes);
             return this;
         }
 
         public HoneycombSdk Build()
         {
-            if (string.IsNullOrWhiteSpace(apiKey))
+            if (string.IsNullOrWhiteSpace(_apiKey))
                 throw new ArgumentException("API key cannot be empty");
             if (string.IsNullOrWhiteSpace("dataset"))
                 throw new ArgumentException("Dataset cannot be empty");
 
             var sdk = Sdk.CreateTracerProviderBuilder()
-                .SetSampler(sampler)
-                .SetResourceBuilder(resourceBuilder)
+                .SetSampler(_sampler)
+                .SetResourceBuilder(_resourceBuilder)
                 .AddOtlpExporter(otlpOptions =>
                 {
-                    otlpOptions.Endpoint = endpoint;
-                    otlpOptions.Headers = string.Format($"x-honeycomb-team={apiKey},x-honeycomb-dataset={dataset}");
+                    otlpOptions.Endpoint = _endpoint;
+                    otlpOptions.Headers = string.Format($"x-honeycomb-team={_apiKey},x-honeycomb-dataset={_dataset}");
                 })
                 // TODO: Add custom HNY env var Decector:
                 // https://github.com/open-telemetry/opentelemetry-dotnet/blob/6b7f2dd77cf9d37260a853fcc95f7b77e296065d/src/OpenTelemetry/Resources/IResourceDetector.cs
