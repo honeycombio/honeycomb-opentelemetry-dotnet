4487f74e9fcea6979f63b72f15f7272eb03ed1e8
Mike Goldsmth <goldsmith.mike@gmail.com>
2021-07-02 16:31:19 +0100

    add console app example

---
+++ examples/console/Program.cs
@@ -0,0 +1,32 @@
+﻿using OpenTelemetry.Trace;
+using Honeycomb.OpenTelemetry;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string AppName = "my-app";
+
+            // configure OpenTelemetry SDK
+            var provider = OpenTelemetry.Sdk.CreateTracerProviderBuilder()
+                .UseHoneycomb(options => {
+                    options.ServiceName = AppName;
+                    options.ApiKey = "{api-key}";
+                    options.Dataset = "{dataset}";
+                })
+                .Build();
+            
+            // create span to describe some application logic
+            var tracer = provider.GetTracer(AppName);
+            using (var span = tracer.StartActiveSpan("doSomething"))
+            {
+                span.SetAttribute("user_id", 123);
+            }   
+
+            // dispose of provider to ensure spans are fluhsed and resoruces are cleaned up
+            provider.Dispose();
+        }
+    }
+}
---
+++ examples/console/README.md
@@ -0,0 +1,9 @@
+# Honeycomb.OpenTelemetry Console example
+
+This example shows how to use the Honeycomb.OpenTelemetry distro with a .NET console application.
+
+To run the example, you need to set your Honeycomb API key and a dataset name in [program.cs](Program.cs), then you can run the example with:
+
+```bash
+dotnet run
+```
