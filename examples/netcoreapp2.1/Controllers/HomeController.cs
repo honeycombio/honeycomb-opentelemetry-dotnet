368de10472d0aa4e93a47ef9126cf54ed4e45d8e
Mike Goldsmith <goldsmith.mike@gmail.com>
2021-08-26 16:00:26 +0100

    Add support for netcoreapp2.1 (#99)
    Adds support for netcoreapp2.1 runtime.
    
    NOTE: This PR doesn't add support ASP.NET Core instrumentation, but does simplify the configuration process and registers a Tracer into DI that can be injected into controllers that can be used to create custom spans.
    
    - Update csproj to add netcoreapp2.1 target framework, define new NETCOREAPP2_1 constant and select appropriate Microsoft.Extensions.Configuration packages
    - Update IServiceCollection extension methods to use both NETSTANDARD2_0 and NETCOREAPP2_1 constants
    - Add example netcoreapp2.1 app
    
    Co-authored-by: JamieDanielson <jamieedanielson@gmail.com>

---
+++ examples/netcoreapp2.1/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using netcoreapp2._1.Models;
+using OpenTelemetry.Trace;
+
+namespace netcoreapp2._1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly Tracer _tracer;
+
+        public HomeController(Tracer tracer)
+        {
+            _tracer = tracer;
+        }
+
+        public IActionResult Index()
+        {
+            using (var span = _tracer.StartActiveSpan("index"))
+            {
+                span.SetAttribute("foo", "bar");
+            }
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
