62c303c7fff9d8959cc61673dddcce0537e957c7
Mike Goldsmth <goldsmith.mike@gmail.com>
2022-06-15 10:48:22 +0100

    always set dataset for metrics exporter

--- src/Honeycomb.OpenTelemetry/HoneycombOptionsExtensions.cs
+++ src/Honeycomb.OpenTelemetry/HoneycombOptionsExtensions.cs
@@ -6,37 +6,30 @@ namespace Honeycomb.OpenTelemetry {
         private const string OtlpVersion = "0.16.0";
 
         internal static string GetTraceHeaders(this HoneycombOptions options) {
-            return GetHeaders(
-                options.IsTracesLegacyKey(),
-                options.TracesApiKey,
-                options.TracesDataset
-            );
-        }
-
-        internal static string GetMetricsHeaders(this HoneycombOptions options) {
-            return GetHeaders(
-                options.IsMetricsLegacyKey(),
-                options.MetricsApiKey,
-                options.MetricsDataset
-            );
-        }
-
-        private static string GetHeaders(bool isLegacyKey, string apikey, string dataset) {
             var headers = new List<string>
             {
                 $"x-otlp-version={OtlpVersion}",
-                $"x-honeycomb-team={apikey}"
+                $"x-honeycomb-team={options.TracesApiKey}"
             };
-            if (isLegacyKey) {
+            if (options.IsTracesLegacyKey()) {
                 // if the key is legacy, add dataset to the header
-                if (!string.IsNullOrWhiteSpace(dataset)) {
-                    headers.Add($"x-honeycomb-dataset={dataset}");
+                if (!string.IsNullOrWhiteSpace(options.TracesDataset)) {
+                    headers.Add($"x-honeycomb-dataset={options.TracesDataset}");
                 } else {
                     // if legacy key and missing dataset, warn on missing dataset
                     Console.WriteLine($"WARN: {EnvironmentOptions.GetErrorMessage("dataset", "HONEYCOMB_DATASET")}.");
                 }
             }
+            return string.Join(",", headers);
+        }
 
+        internal static string GetMetricsHeaders(this HoneycombOptions options) {
+            var headers = new List<string>
+            {
+                $"x-otlp-version={OtlpVersion}",
+                $"x-honeycomb-team={options.MetricsApiKey}",
+                $"x-honeycomb-dataset={options.MetricsDataset}"
+            };
             return string.Join(",", headers);
         }
     }
