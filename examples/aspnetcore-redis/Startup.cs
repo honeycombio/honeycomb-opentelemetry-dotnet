3321485ba8e973437a94f4b990cfea9180cdfd0d
Jamie Danielson <jamieedanielson@gmail.com>
2022-09-13 10:54:48 -0400

    docs: Add Redis instrumentation example app (#242)
    * add separate app to show redis instrumentation
    A bit of cleanup on the aspnetcore app, and added a new app that is similar but removes metrics and adds redis instrumentation.

---
+++ examples/aspnetcore-redis/Startup.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Honeycomb.OpenTelemetry;
+using StackExchange.Redis;
+
+namespace aspnetcoreredis
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            // configure OpenTelemetry SDK to send data to Honeycomb
+            services.AddHoneycomb(Configuration);
+
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(
+            new ConfigurationOptions
+            {
+                EndPoints = { "localhost:6379" },
+                AbortOnConnectFail = false, // allow for reconnects if redis is not available
+            });
+            services.AddSingleton<IConnectionMultiplexer>(redis);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
