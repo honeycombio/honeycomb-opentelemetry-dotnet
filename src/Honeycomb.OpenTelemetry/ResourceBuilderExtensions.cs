d31dbf03ea547c6e8190284af1fdd2d528661666
cartermp <pcarter@fastmail.com>
2022-09-14 14:58:14 -0700

    Attempt to match Java distro resources

--- src/Honeycomb.OpenTelemetry/ResourceBuilderExtensions.cs
+++ src/Honeycomb.OpenTelemetry/ResourceBuilderExtensions.cs
@@ -1,5 +1,6 @@
 using OpenTelemetry.Resources;
 using System;
+using System.Runtime.InteropServices;
 using System.Collections.Generic;
 using System.Reflection;
 
@@ -22,6 +23,56 @@ namespace Honeycomb.OpenTelemetry
                     new KeyValuePair<string, object>("honeycomb.distro.version", GetFileVersion()),
                     new KeyValuePair<string, object>("honeycomb.distro.runtime_version",
                         Environment.Version.ToString()),
+                })
+                .AddRuntimeResource()
+                .AddOSResource();
+        }
+
+        private static ResourceBuilder AddRuntimeResource(this ResourceBuilder builder)
+        {
+            var frameworkDescription = RuntimeInformation.FrameworkDescription;
+            var parts = frameworkDescription.Split(' ');
+            return builder
+                .AddAttributes(new List<KeyValuePair<string, object>>
+                {
+                    // See here:
+                    // https://docs.microsoft.com/en-us/dotnet/api/system.runtime.interopservices.runtimeinformation.frameworkdescription?view=netstandard-2.0
+                    // Description likely cannot be synthesized, at least not easily.
+                    new KeyValuePair<string, object>("process.runtime.name", parts[0]),
+                    new KeyValuePair<string, object>("process.runtime.version", parts[1])
+                });
+        }
+
+        private static ResourceBuilder AddOSResource(this ResourceBuilder builder)
+        {
+            var osType = "unknown";
+            var osName = "unknown";
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                osType = "windows";
+                osName = "Windows";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                osType = "linux";
+                osName = "Linux";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                osType = "darwin";
+                osName = "macOS";
+            }
+
+            var versionWithPotentialWindowsServicePack = Environment.OSVersion.VersionString.IndexOf(" ") + 1;
+            return builder
+                .AddAttributes(new List<KeyValuePair<string, object>>
+                {
+                    new KeyValuePair<string, object>("os.type", osType),
+                    new KeyValuePair<string, object>("os.description",
+                        Environment.OSVersion.VersionString),
+                    new KeyValuePair<string, object>("os.name", osName),
+                    new KeyValuePair<string, object>("os.version",
+                        Environment.OSVersion.VersionString.Substring(versionWithPotentialWindowsServicePack)),
                 });
         }
 
