e2f580231c7ca5752fe9846f8c22d156870d5f07
Mike Goldsmth <goldsmith.mike@gmail.com>
2022-09-21 17:32:23 +0100

    configure honeycomb options as part of setup

--- examples/blazor-wasm/Program.cs
+++ examples/blazor-wasm/Program.cs
@@ -4,11 +4,23 @@ using blazor_wasm;
 using OpenTelemetry.Trace;
 using Honeycomb.OpenTelemetry;
 
+const string appName = "blazor-example";
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddOpenTelemetryTracing(builder => builder.AddHoneycomb());
+
+builder.Services.AddOpenTelemetryTracing(builder =>
+{
+    builder.AddHoneycomb(options =>
+    {
+        options.ServiceName = appName;
+        options.ApiKey = "{apikey}";
+    });
+    builder.AddConsoleExporter();
+});
+builder.Services.AddSingleton(TracerProvider.Default.GetTracer(appName));
 
 await builder.Build().RunAsync();
