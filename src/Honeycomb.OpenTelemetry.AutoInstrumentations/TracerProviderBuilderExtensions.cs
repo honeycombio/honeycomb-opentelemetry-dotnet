5337b4574630db274a2f6b7a17c3ae31534d18ed
Phillip Carter <pcarter@fastmail.com>
2022-09-23 16:35:39 +0200

    Add Honeycomb.OpenTelemetry.Autoinstrumentations (#270)

---
+++ src/Honeycomb.OpenTelemetry.AutoInstrumentations/TracerProviderBuilderExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Npgsql;
+using OpenTelemetry.Trace;
+
+namespace Honeycomb.OpenTelemetry.AutoInstrumentations
+{
+    /// <summary>
+    /// Extension methods to add instrumentation support for many common instrumentation packages.
+    /// </summary>
+    public static class TracerProviderBuilderExtensions
+    {
+        /// <summary>
+        /// Adds support for common instrumentation packages. Refer to documentation for a complete list of included instrumentations.
+        /// </summary>
+        /// <param name="builder"><see cref="TracerProviderBuilder"/> being configured.</param>
+        /// <returns>The instance of <see cref="TracerProviderBuilder"/> to chain the calls.</returns>
+        public static TracerProviderBuilder AddAllInstrumentation(this TracerProviderBuilder builder)
+        {
+            return
+                builder
+#if NET6_0_OR_GREATER
+                .AddAspNetCoreInstrumentationWithBaggage()
+                .AddGrpcClientInstrumentation()
+#elif NET462_OR_GREATER
+                .AddAspNetInstrumentation()
+#endif
+#if NETSTANDARD2_0_OR_GREATER
+                .AddQuartzInstrumentation()
+#endif
+                .AddHttpClientInstrumentation()
+                .AddSqlClientInstrumentation()
+                .AddEntityFrameworkCoreInstrumentation()
+                .AddMySqlDataInstrumentation()
+                .AddRedisInstrumentation()
+                .AddWcfInstrumentation()
+                .AddNpgsql();
+        }
+    }
+}
