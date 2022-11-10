704c17180ff662bebc29b102c69900539aebc14b
Patrick Williams <patrickwilliams@quickenloans.com>
2022-11-09 20:33:40 -0500

    PR Feedback
    Maintain existing ConfigurationManager based functionality

--- src/Honeycomb.OpenTelemetry/WebApplicationBuilderExtensions.cs
+++ src/Honeycomb.OpenTelemetry/WebApplicationBuilderExtensions.cs
@@ -3,6 +3,22 @@ using Honeycomb.OpenTelemetry;
 namespace Microsoft.Extensions.Configuration
 {
     /// <summary>
+    /// Extension methods for <see cref="ConfigurationManager"/> to help configure Honeycomb with OpenTelemetry.
+    /// </summary>
+    public static class ConfigurationManagerExtensions
+    {
+        /// <summary>
+        /// Attempts to retrieve an instance of <see cref="HoneycombOptions"/> used to configure the OpenTelemetry SDK.
+        /// </summary>
+        public static HoneycombOptions GetHoneycombOptions(this ConfigurationManager builder)
+        {
+            return builder
+                .GetSection(HoneycombOptions.ConfigSectionName)
+                .Get<HoneycombOptions>();
+        }
+    }
+
+    /// <summary>
     /// Extension methods for <see cref="IConfiguration"/> to help configure Honeycomb with OpenTelemetry.
     /// </summary>
     public static class ConfigurationInterfaceExtensions
