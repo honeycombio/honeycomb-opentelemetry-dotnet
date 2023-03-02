0583a6813865f6eff1d91f006bc44271c9f8ce47
JamieDanielson <jamieedanielson@gmail.com>
2023-03-02 10:35:56 -0500

    update tests with new otlp version

--- test/Honeycomb.OpenTelemetry.Tests/HoneycombOptionsExtensionsTests.cs
+++ test/Honeycomb.OpenTelemetry.Tests/HoneycombOptionsExtensionsTests.cs
@@ -7,11 +7,11 @@ namespace Honeycomb.OpenTelemetry {
         private const string ClassicApiKey = "a142c03cf06936628e60f4c6157fde46";
 
         [Theory]
-        [InlineData("", "", "x-otlp-version=0.18.0,x-honeycomb-team=")]
-        [InlineData(ModernApiKey, "", "x-otlp-version=0.18.0,x-honeycomb-team=6936628e60f4c6157fde46")]
-        [InlineData(ModernApiKey, "dataset", "x-otlp-version=0.18.0,x-honeycomb-team=6936628e60f4c6157fde46")]
-        [InlineData(ClassicApiKey, "", "x-otlp-version=0.18.0,x-honeycomb-team=a142c03cf06936628e60f4c6157fde46")]
-        [InlineData(ClassicApiKey, "dataset", "x-otlp-version=0.18.0,x-honeycomb-team=a142c03cf06936628e60f4c6157fde46,x-honeycomb-dataset=dataset")]
+        [InlineData("", "", "x-otlp-version=0.19.0,x-honeycomb-team=")]
+        [InlineData(ModernApiKey, "", "x-otlp-version=0.19.0,x-honeycomb-team=6936628e60f4c6157fde46")]
+        [InlineData(ModernApiKey, "dataset", "x-otlp-version=0.19.0,x-honeycomb-team=6936628e60f4c6157fde46")]
+        [InlineData(ClassicApiKey, "", "x-otlp-version=0.19.0,x-honeycomb-team=a142c03cf06936628e60f4c6157fde46")]
+        [InlineData(ClassicApiKey, "dataset", "x-otlp-version=0.19.0,x-honeycomb-team=a142c03cf06936628e60f4c6157fde46,x-honeycomb-dataset=dataset")]
         public void TracesHeaders(string apikey, string dataset, string expectedHeader) {
             var options = new HoneycombOptions
             {
@@ -22,11 +22,11 @@ namespace Honeycomb.OpenTelemetry {
         }
 
         [Theory]
-        [InlineData("", "", "x-otlp-version=0.18.0,x-honeycomb-team=,x-honeycomb-dataset=")]
-        [InlineData(ModernApiKey, "", "x-otlp-version=0.18.0,x-honeycomb-team=6936628e60f4c6157fde46,x-honeycomb-dataset=")]
-        [InlineData(ModernApiKey, "dataset", "x-otlp-version=0.18.0,x-honeycomb-team=6936628e60f4c6157fde46,x-honeycomb-dataset=dataset")]
-        [InlineData(ClassicApiKey, "", "x-otlp-version=0.18.0,x-honeycomb-team=a142c03cf06936628e60f4c6157fde46,x-honeycomb-dataset=")]
-        [InlineData(ClassicApiKey, "dataset", "x-otlp-version=0.18.0,x-honeycomb-team=a142c03cf06936628e60f4c6157fde46,x-honeycomb-dataset=dataset")]
+        [InlineData("", "", "x-otlp-version=0.19.0,x-honeycomb-team=,x-honeycomb-dataset=")]
+        [InlineData(ModernApiKey, "", "x-otlp-version=0.19.0,x-honeycomb-team=6936628e60f4c6157fde46,x-honeycomb-dataset=")]
+        [InlineData(ModernApiKey, "dataset", "x-otlp-version=0.19.0,x-honeycomb-team=6936628e60f4c6157fde46,x-honeycomb-dataset=dataset")]
+        [InlineData(ClassicApiKey, "", "x-otlp-version=0.19.0,x-honeycomb-team=a142c03cf06936628e60f4c6157fde46,x-honeycomb-dataset=")]
+        [InlineData(ClassicApiKey, "dataset", "x-otlp-version=0.19.0,x-honeycomb-team=a142c03cf06936628e60f4c6157fde46,x-honeycomb-dataset=dataset")]
         public void MetricsHeaders(string apikey, string dataset, string expectedHeader) {
             var options = new HoneycombOptions
             {
